e2208898243dec3db57ea44d9c25117257a831a9
Hajime Hoshi <hajimehoshi@gmail.com>
2018-06-26 03:06:08 +0900

    Update API

--- js/js_notwasm.go
+++ js/js_notwasm.go
@@ -13,11 +13,17 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
-var (
-	Undefined = Value{v: js.Undefined}
-	Null      = Value{v: nil}
-	Global    = Value{v: js.Global}
-)
+func Global() Value {
+	return Value{v: js.Global}
+}
+
+func Null() Value {
+	return Value{v: nil}
+}
+
+func Undefined() Value {
+	return Value{v: js.Undefined}
+}
 
 type Callback struct {
 	f     func([]Value)
@@ -106,7 +112,7 @@ func ValueOf(x interface{}) Value {
 			}),
 		}
 	case nil:
-		return Null
+		return Null()
 	case bool, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, float32, float64, unsafe.Pointer, string, []byte:
 		return Value{v: id.Invoke(x)}
 	case []int8, []int16, []int32, []int64, []uint16, []uint32, []uint64, []float32, []float64:
