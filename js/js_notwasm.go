dd289ccb2dc401b8d323b9d9b6bf03a101e07a97
Elliott Stoneham <elliott.stoneham@gmail.com>
2018-06-27 20:34:49 +0100

    Remove "fmt" dependency in js_notwasm.go
    Reduces the generated JS for a small project by around 1Mb

--- js/js_notwasm.go
+++ js/js_notwasm.go
@@ -7,7 +7,7 @@
 package js
 
 import (
-	"fmt"
+	"reflect"
 	"unsafe"
 
 	"github.com/gopherjs/gopherjs/js"
@@ -118,7 +118,7 @@ func ValueOf(x interface{}) Value {
 	case []int8, []int16, []int32, []int64, []uint16, []uint32, []uint64, []float32, []float64:
 		return Value{v: id.Invoke(x)}
 	default:
-		panic(fmt.Sprintf("invalid arg: %T", x))
+		panic(`invalid arg: ` + reflect.TypeOf(x).String())
 	}
 }
 
